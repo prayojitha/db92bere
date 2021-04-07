24f3b010b5486e801912bbef9ba8fd12beb7b4d2
prayojitha <prayojitha.bere@gmail.com>
2021-04-06 20:37:09 -0500

    modified electronic.js

--- routes/electronic.js
+++ routes/electronic.js
@@ -3,6 +3,6 @@ var router = express.Router();
 const electronic_controlers = require("../controllers/electronic");
 
 /* GET electronic */
-router.get('/', electronic_controlers.mobile_view_all_Page);
+router.get('/', electronic_controlers.electronic_view_all_Page);
 
 module.exports = router;
