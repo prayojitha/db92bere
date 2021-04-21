b62d00bb922566aa8e507bed9c8653a45acb3de9
prayojitha <prayojitha.bere@gmail.com>
2021-04-20 22:54:11 -0500

    authentication and protecting endpoints

--- routes/electronic.js
+++ routes/electronic.js
@@ -6,7 +6,16 @@ const electronic_controlers = require("../controllers/electronic");
 router.get("/", electronic_controlers.electronic_view_all_Page);
 router.get("/electronicdetail", electronic_controlers.electronic_view_one_Page);
 router.get("/electroniccreate", electronic_controlers.electronic_create_Page);
-router.get("/electronicupdate", electronic_controlers.electronic_update_Page);
+const secured = (req, res, next) => {
+    if (req.user){
+        return next();
+    }
+    req.session.returnTo= req.originalUrl;
+    res.redirect("/login");
+}
+router.get("/electronicupdate",secured, electronic_controlers.electronic_update_Page);
 router.get("/electronicdelete", electronic_controlers.electronic_delete_Page);
+
+
 
 module.exports = router;
