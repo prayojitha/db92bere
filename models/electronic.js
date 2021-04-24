60c9ccbf4f53d83b47eb3160c8ef6fcd77ef65c0
prayojitha <prayojitha.bere@gmail.com>
2021-04-23 22:43:54 -0500

    updating buttons and displaying collection items

--- models/electronic.js
+++ models/electronic.js
@@ -1,7 +1,38 @@
 const mongoose = require("mongoose");
-const electronicSchema = mongoose.Schema({
-category: String,
+const Validator = require("validator")
+const electronicSchema =new  mongoose.Schema({
+// category: String,
+category: {
+    type:String,
+    lowercase: true,
+    // enum:["Fred", "Barney"],
+    minlength: [2,"minimum 2 letters"],
+    maxlength: 10,
+    required: true,
+    errmsg: "minimum length should be 2",
+    },
 item: String,
-price: Number,
+price: {
+    type:Number,
+    min:[1, "too small"],
+    max:25,
+
+}
 })
 module.exports = mongoose.model("electronic", electronicSchema);
+
+// const userSchema= new mongoose.Schema({
+//     category: {
+//         type:String,
+//         lowercase: true,
+//         enum:["Fred", "Barney"],
+//         required:[true,l]
+//         }
+    // age: {
+    //         type:Number,
+    //         set: v => Math.floor(v), 
+    //         default: 10,
+    //         min:[1, "too small"]
+    //         max:25,
+    // }
+// });
--- routes/electronic.js
+++ routes/electronic.js
@@ -5,7 +5,6 @@ const electronic_controlers = require("../controllers/electronic");
 /* GET electronic */
 router.get("/", electronic_controlers.electronic_view_all_Page);
 router.get("/electronicdetail", electronic_controlers.electronic_view_one_Page);
-router.get("/electroniccreate", electronic_controlers.electronic_create_Page);
 const secured = (req, res, next) => {
     if (req.user){
         return next();
@@ -13,8 +12,10 @@ const secured = (req, res, next) => {
     req.session.returnTo= req.originalUrl;
     res.redirect("/login");
 }
+router.get("/electroniccreate",secured, electronic_controlers.electronic_create_Page);
+
 router.get("/electronicupdate",secured, electronic_controlers.electronic_update_Page);
-router.get("/electronicdelete", electronic_controlers.electronic_delete_Page);
+router.get("/electronicdelete",secured, electronic_controlers.electronic_delete_Page);
 
 
 
